3fc93355d53de8c32166c54c4cfc446f8c4842d2
xDEVaz <danielvaz.tzk@hotmail.com>
2023-12-12 09:10:48 -0300

    Criando a classe posical

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace ChessGame
+﻿using Tabuleiro;
+
+namespace ChessGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posicao "+ P);
+
         }
     }
 }
