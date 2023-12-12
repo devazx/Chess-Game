5e29f065816680e3e4f13d7794aedc4414872ccc
xDEVaz <danielvaz.tzk@hotmail.com>
2023-12-12 14:49:49 -0300

    Criando a classe posicao chess

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -10,21 +10,10 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-            try
-            {
-                TabuleiroF tab = new TabuleiroF(8, 8);
+            PosicaoChess pos = new PosicaoChess('a', 1);
+            Console.WriteLine(pos);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                Tela.imprimirTabuleiro(tab);
-                                
-            }
-            catch (TabuleiroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Console.WriteLine(pos.toPosicao());
             Console.ReadLine();
 
         }
